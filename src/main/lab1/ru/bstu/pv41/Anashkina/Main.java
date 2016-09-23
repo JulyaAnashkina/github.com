24d06aa01f309c05712937fb9f03f355d772b0a4
JulyaAnashkina <julyaanashkin@yandex.ru>
2016-09-23 11:57:32 +0300

    New project

---
+++ src/main/lab1/ru/bstu/pv41/Anashkina/Main.java
@@ -0,0 +1,11 @@
+package ru.bstu.pv41.Anashkina;
+
+/**
+ * Created by Юля on 23.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello");
+    }
+}
